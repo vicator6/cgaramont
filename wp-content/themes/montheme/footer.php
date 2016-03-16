67bd8775ec84c5c704b7e6e2615928f966ded394
vicator6 <vicator6@gmail.com>
2016-03-16 13:52:10 +0100

    accueil + carre anim

--- wp-content/themes/montheme/footer.php
+++ wp-content/themes/montheme/footer.php
@@ -51,6 +51,11 @@
         </div>
     </div>
 </footer>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="<?php bloginfo( 'template_directory' ); ?>/js/bootstrap.min.js"></script>
 </body>
 <?php wp_footer(); ?>
 
